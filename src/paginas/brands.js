e4bca577565a6c5bc9d301225f08dad418616dae
Gerarvvdor <00053520@uca.edu.sv>
2023-03-15 12:05:59 -0600

    Third update

--- src/paginas/brands.js
+++ src/paginas/brands.js
@@ -12,24 +12,27 @@ const Brands = () => {
         <>
         <h1>MARCAS</h1>
         <div>
-            <p><a href="https://www.reliablecontrols.com/" >Reliable Controls</a> Es una Corporación dedicada a la venta de productos y servicios en la industria de automatización de edificios. Fundada en 1986 en Surrey, Columbia Británica, Canadá. Actualmente la sede corporativa se encuentra en Victoria, BC con soporte de ventas en ubicaciones en todo el mundo. </p>
+            <p><a href="https://www.reliablecontrols.com/"target="_blank">Reliable Controls</a> Es una Corporación dedicada a la venta de productos y servicios en la industria de automatización de edificios. Fundada en 1986 en Surrey, Columbia Británica, Canadá. Actualmente la sede corporativa se encuentra en Victoria, BC con soporte de ventas en ubicaciones en todo el mundo. </p>
             <img src={Reliable} alt=""/>
 
         </div>
         <div>
-            <p><a href="https://www.lennox.com/">Lennox International Inc. </a>Es un proveedor de productos de control climático para los mercados de calefacción, ventilación, aire acondicionado y refrigeración. fundada en 1895, en Marshalltown, Iowa, por Dave Lennox, el cual desarrollo numerosos avances en las soluciones de calefacción, refrigeración y control climático.</p>
+            <p><a href="https://www.lennox.com/" target="_blank">Lennox International Inc. </a>Es un proveedor de productos de control climático para los mercados de calefacción, ventilación, aire acondicionado y refrigeración. fundada en 1895, en Marshalltown, Iowa, por Dave Lennox, el cual desarrollo numerosos avances en las soluciones de calefacción, refrigeración y control climático.</p>
         </div>
         <div>
             <p>York es el nombre de una compañía que comenzó en York, Pennsylvania, EE. UU., En 1874, que desarrolló la marca York de equipos de refrigeración y HVAC. La marca York ahora es propiedad (desde 2005) de <a href="http://www.johnsoncontrols.com/">Jhhnson Controls </a> una compañía americana que ofrece productos y servicios para optimizar la eficiencia energética en edificios.</p>
             <img src={Lennox} alt=""/>
         </div>
         <div>
+            <p><a href="https://www.belimo.com/us/en_US/" target="_blank">Belimo</a> Es líder mundial en el mercado en el desarrollo, producción y comercialización de soluciones de actuadores para controlar sistemas de calefacción, ventilación y aire acondicionado. ofrece innovaciones en comodidad,eficiencia energética y soluciones de seguridad.</p>
             <img src={Belimo} alt=""/> 
         </div>
         <div>
+            <p><a href="https://www.functionaldevices.com/" target="_blank">Functional Devices, Inc</a> ha estado diseñando y fabricando dispositivos electrónicos de calidad en los Estados Unidos de América desde 1969. se ha establecido como líder en HVAC, controles de edificios, gestión de energía, ahorro de energía, controles de iluminación e industrias inalámbricas.</p>
             <img src={Rib} alt="" />
         </div>
         <div>
+            <p><a href="https://www.adelte.com/es/" target="_blank"></a></p>
             <img src={Adelte} alt="" />
         </div>
     
--- src/paginas/contacts.js
+++ src/paginas/contacts.js
@@ -3,6 +3,11 @@ import React from "react";
 const Contacts = () => {
     return (
         <>
+        <div>
+            <h1>CONTACTOS</h1>
+            <h2>Dirección oficinas</h2>
+            <h3>Colonia San Francisco /n 77, Calle los castaños</h3>
+        </div>
         </>
 
     );
